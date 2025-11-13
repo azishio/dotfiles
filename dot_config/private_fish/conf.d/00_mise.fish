593d433b011d6aac44fa9bc80d29d52bf746b013
azishio <127939746+azishio@users.noreply.github.com>
2025-11-14 02:36:37 +0900

    Add dot_config

---
+++ dot_config/private_fish/conf.d/00_mise.fish
@@ -0,0 +1,39 @@
+# ============================================================
+# mise 用 fish alias セット（追加オプション重視・パイプ禁止）
+# ============================================================
+~/.local/bin/mise activate fish | source
+
+# --- ls 系（eza）
+if type -q eza
+    # *ユーザーが追加オプションを書けるように alias の後ろ何もつけない*
+    alias ls="eza -h --icons --group-directories-first"
+    alias la="eza -ha --icons --group-directories-first"
+    alias ll="eza -lha --icons --group-directories-first"
+
+    # tree 互換
+    alias tree="eza --tree -h --icons --group-directories-first"
+end
+
+# --- grep → ripgrep
+if type -q rg
+    alias grep="rg"
+end
+
+# --- find → fd
+if type -q fd
+    alias find="fd"
+end
+
+# --- df, du, free（-h だけ固定）
+if type -q df
+    alias df="df -h"
+end
+
+if type -q du
+    alias du="du -h"
+end
+
+if type -q free
+    alias free="free -h"
+end
+
---
+++ dot_config/private_fish/conf.d/01_terminal.fish
@@ -0,0 +1,36 @@
+if status is-interactive
+	# Starship起動
+	if type -q starship
+        starship init fish | source
+    end
+
+	if not set -q ZELLIJ
+	    # テーマ一覧（1行1テーマ）
+        set themes \
+        	ansi ao atelier-sulphurpool ayu_mirage ayu_dark \
+            catppuccin-frappe catppuccin-macchiato cyber-noir \
+        	blade-runner retro-wave dracula everforest-dark \
+            gruvbox-dark iceberg-dark kanagawa lucario menace \
+            molokai-dark night-owl nightfox nord one-half-dark \
+            onedark solarized-dark tokyo-night-dark \
+            tokyo-night-storm tokyo-night vesper
+
+        # 使うホストID
+        set host_id (hostname)
+
+        # --- Linux 標準のハッシュ関数 ---
+        # sha256sum → 16進文字列 → 先頭8桁を整数に変換
+        set hash (echo -n $host_id | sha256sum | cut -c 1-8)
+        
+		# 16進 → 10進（printf は POSIX の標準機能）
+        set num (printf "%d" 0x$hash)
+        
+		set count (count $themes)
+
+        set idx (math "$num % $count")
+
+        set selected_theme $themes[$idx]
+
+        exec zellij options --theme $selected_theme
+    end
+end
