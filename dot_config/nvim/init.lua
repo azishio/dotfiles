58fe6c684aeb5fef791027e3edcd7e2a3e3413dd
azishio <127939746+azishio@users.noreply.github.com>
2025-11-14 11:21:38 +0900

    Update .chezmoi.toml.tmpl Add .config/nvim/README.md Add .config/nvim/.luarc.json Add .config/nvim/.neoconf.json Add .config/nvim/.stylua.toml Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/community.lua Add .config/nvim/lua/lazy_setup.lua Add .config/nvim/lua/plugins/astrocore.lua Add .config/nvim/lua/plugins/astrolsp.lua Add .config/nvim/lua/plugins/astroui.lua Add .config/nvim/lua/plugins/mason.lua Add .config/nvim/lua/plugins/none-ls.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/user.lua Add .config/nvim/lua/polish.lua Add .config/nvim/neovim.yml Add .config/nvim/selene.toml

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,51 @@
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
+
+
+-- 自動インデント
+vim.opt.autoindent = true
+
+-- タブ幅
+vim.opt.tabstop = 4
+
+-- 自動インデント幅
+vim.opt.shiftwidth = 4
+
+-- タブをスペースに展開しない
+vim.opt.expandtab = false
+
+-- タブ幅(入力時)
+vim.opt.softtabstop = 0
+
+-- 行番号表示
+vim.opt.number = true
+
+-- シンタックスハイライト
+vim.cmd("syntax on")
+
+
